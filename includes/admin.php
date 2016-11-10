89654fb3134ec22bc0747a5057a81a572cf118c1
Josh Eaton <josh@josheaton.org>
2016-11-10 15:36:12 -0500

    Register setting

--- includes/admin.php
+++ includes/admin.php
@@ -7,6 +7,11 @@
 
 namespace Civil_Comments;
 
+add_action( 'admin_init', __NAMESPACE__ . '\\register_settings' );
+function register_settings() {
+	register_setting( 'civil_comments', 'civil_comments' );
+}
+
 add_action( 'admin_menu', __NAMESPACE__ . '\\add_settings_page' );
 function add_settings_page() {
 	add_submenu_page(
--- templates/settings.php
+++ templates/settings.php
@@ -1,3 +1,11 @@
+<?php
+if ( ! current_user_can( 'manage_options' ) ) {
+	return;
+}
+
+if ( isset( $_GET['settings-updated'] ) ) {
+	add_settings_error( 'civil_comments_messages', 'civil_comments_message', __( 'Settings Saved' ), 'updated' );
+} ?>
 <div class="wrap">
 	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 </div>
